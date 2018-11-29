0225e3dfcd94c1d84c914d98229ff33ee2dd6f1d
glucee <glucee@hotmail.com>
2018-11-29 16:24:56 +0000

    put two methods together

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
-#include "..\lib\capsense\capsense.h"
+#include "../lib/capsense/capsense.h"
+//#include "../lib/tilt/tilt.h"
 
 void setup()
 {
   setup_capsense();
+  //setup_tilt();
 }
 
-void loop(){
+void loop()
+{
   run_capsense();
+  //run_tilt();
+  delay(100);
 }
